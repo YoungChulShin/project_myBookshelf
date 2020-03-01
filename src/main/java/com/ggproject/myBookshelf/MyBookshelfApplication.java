7bdd4cb73e2ae3d102cd80a4d563b48f92fd7c16
YoungChulShin <go1323@gmail.com>
2020-03-02 06:22:03 +0900

    프로젝트 추가ㅣ

---
+++ src/main/java/com/ggproject/myBookshelf/MyBookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.ggproject.myBookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggproject/myBookshelf/MyBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggproject.myBookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
